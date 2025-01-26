2ed8281acf0c97bf4e190a2499fb878dee8d268d
User224839 <serikbajn18@gmail.com>
2025-01-26 09:32:16 +0500

    2.3

--- lab1/1.py
+++ lab1/1.py
@@ -1,35 +1,11 @@
-thislist = ["apple", "banana", "cherry"]
-print(thislist)
+thistuple = ("apple",)
+print(type(thistuple))
 
-list1 = ["apple", "banana", "cherry"]
-list2 = [1, 5, 7, 9, 3]
-list3 = [True, False, False]
-#insert kak v c++ XD
-thislist = ["apple", "banana", "cherry"]
-thislist.insert(2, "watermelon")
-print(thislist)
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
 
-
-thislist = ["apple", "banana", "cherry"]
-tropical = ["mango", "pineapple", "papaya"]
-thislist.extend(tropical)
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-for i in range(len(thislist)):
-  print(thislist[i])
-
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
-
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
-
-print(newlist)
-
-
-#reverse no ne sort
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort(reverse = True)
-print(thislist)
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
