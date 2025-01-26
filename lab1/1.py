4040454a9b9ba06604a753f4351324067819dc66
User224839 <serikbajn18@gmail.com>
2025-01-26 09:29:57 +0500

    2.2

--- lab1/1.py
+++ lab1/1.py
@@ -1,15 +1,35 @@
-bool(False)
-bool(None)
-bool(0)
-bool("")
-bool(())
-bool([])
-bool({})
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
 
-def myFunction() :
-  return True
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+#insert kak v c++ XD
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
 
-if myFunction():
-  print("YES!")
-else:
-  print("NO!")
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+
+#reverse no ne sort
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
