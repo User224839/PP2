90e726ec691127f6be5a7cbaba1bcd23200b60c1
User224839 <serikbajn18@gmail.com>
2025-01-31 12:16:20 +0500

    3.3

--- lab1/1.py
+++ lab1/1.py
@@ -1,17 +1,4 @@
-def myfunc(n):
-  return lambda a : a * n
-
-mydoubler = myfunc(2)
-
-print(mydoubler(11))
-
-
-#using vriety of func in one place
-def myfunc(n):
-  return lambda a : a * n
-
-mydoubler = myfunc(2)
-mytripler = myfunc(3)
-
-print(mydoubler(11))
-print(mytripler(11))
+cars = ["Ford", "Volvo", "BMW"]
+x = cars[0]
+cars[0] = "Toyota"
+cars.pop(1)
