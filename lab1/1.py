9ffb65e7ca4f68b26b7267b7fcde70235e140b85
User224839 <serikbajn18@gmail.com>
2025-02-08 22:14:08 +0500

    1.1

--- lab1/1.py
+++ lab1/1.py
@@ -1,32 +1,7 @@
-class MyClass:
-  x = 5
-p1 = MyClass()
-print(p1.x)
-
-
 class Person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
-
-p1 = Person("John", 36)
-
-print(p1.name)
-print(p1.age)
-
-class Person:
-  def __init__(mysillyobject, name, age):
-    mysillyobject.name = name
-    mysillyobject.age = age
-
-  def myfunc(abc):
-    print("Hello my name is " + abc.name)
-
-p1 = Person("John", 36)
-p1.myfunc()
-
-
-
 class Student(Person):
   def __init__(self, fname, lname, year):
     super().__init__(fname, lname)
