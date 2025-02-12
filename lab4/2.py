cdacbf0760956dc5d19ba4993bf2662a9181323b
User224839 <serikbajn18@gmail.com>
2025-02-12 12:53:41 +0500

    1.02/3

--- lab4/2.py
+++ lab4/2.py
@@ -1,9 +1,20 @@
-class StringProcessor:
-    def __init__(self):
-        self.string = ""
+class Shape:
+    def area(self):
+        return 0
+
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
     
-    def getString(self):
-        self.string = input("Enter a string: ")
+    def area(self):
+        return self.length ** 2
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
     
-    def printString(self):
-        print(self.string.upper())
+    def area(self):
+        return self.length * self.width
