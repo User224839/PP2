bd7e18ed4b0b08ad3df1b98f9f620361b12f07a5
User224839 <serikbajn18@gmail.com>
2025-02-12 13:22:03 +0500

    1.21/2/3/4/5

--- lab3/2.py
+++ lab3/2.py
@@ -1,16 +1,101 @@
-import random
-def guess_the_number():
-    name = input("Hello! What is your name?\n")
-    number = random.randint(1, 20)
-    print(f"Well, {name}, I am thinking of a number between 1 and 20.")
-    guesses = 0
-    while True:
-        guess = int(input("Take a guess.\n"))
-        guesses += 1
-        if guess < number:
-            print("Your guess is too low.")
-        elif guess > number:
-            print("Your guess is too high.")
-        else:
-            print(f"Good job, {name}! You guessed my number in {guesses} guesses!")
-            break
+# Dictionary of movies
+
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+
+def is_highly_rated(movie):
+    return movie["imdb"] > 5.5
+
+
+def highly_rated_movies(movies):
+    return [movie for movie in movies if is_highly_rated(movie)]
+
+
+def movies_by_category(movies, category):
+    return [movie for movie in movies if movie["category"].lower() == category.lower()]
+
+
+def average_imdb(movies):
+    if not movies:
+        return 0
+    return sum(movie["imdb"] for movie in movies) / len(movies)
+
+
+def average_imdb_by_category(movies, category):
+    category_movies = movies_by_category(movies, category)
+    return average_imdb(category_movies)
