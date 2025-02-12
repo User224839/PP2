52207aedf585f9f755fcee75c49d69c9494389d2
User224839 <serikbajn18@gmail.com>
2025-02-12 13:10:58 +0500

    1.4/5/6

--- lab3/2.py
+++ lab3/2.py
@@ -1,15 +1,19 @@
 import random
 import itertools
 
-def grams_to_ounces(grams):
-    return 28.3495231 * grams
 
-def fahrenheit_to_celsius(fahrenheit):
-    return (5 / 9) * (fahrenheit - 32)
+def filter_prime(numbers):
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+    return list(filter(is_prime, numbers))
 
-def solve(numheads, numlegs):
-    for chickens in range(numheads + 1):
-        rabbits = numheads - chickens
-        if (chickens * 2 + rabbits * 4) == numlegs:
-            return chickens, rabbits
-    return None
+def string_permutations(s):
+    return list(map("".join, itertools.permutations(s)))
+
+def reverse_words(sentence):
+    return " ".join(sentence.split()[::-1])
