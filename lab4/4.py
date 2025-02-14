46d0a8dae83bf33a475dc43c2805ec9ee510da3d
User224839 <serikbajn18@gmail.com>
2025-02-14 12:05:29 +0500

    4.2.3

--- lab4/4.py
+++ lab4/4.py
@@ -1,7 +1,9 @@
-def even_numbers(n):
-    for i in range(0, n + 1, 2):
-        yield i
+def divisible_by_3_and_4(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
 
-# Input from console
-n = int(input("Enter a number: "))
-print(",".join(map(str, even_numbers(n))))
+# Example usage
+n = 50
+for num in divisible_by_3_and_4(n):
+    print(num, end=" ")
