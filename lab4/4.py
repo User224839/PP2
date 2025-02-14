78a4e55d33a8cb75782cec743d4a7ff87b45c6de
User224839 <serikbajn18@gmail.com>
2025-02-14 12:09:47 +0500

    4.3.3

--- lab4/4.py
+++ lab4/4.py
@@ -1,13 +1,13 @@
-def trapezoid_area(height, base1, base2):
-    return 0.5 * (base1 + base2) * height
+import math
+
+def regular_polygon_area(n, side_length):
+    return (n * side_length ** 2) / (4 * math.tan(math.pi / n))
 
 # Example usage
-height = 5
-base1 = 5
-base2 = 6
-area = trapezoid_area(height, base1, base2)
+n = 4  # Number of sides
+side_length = 25
+area = regular_polygon_area(n, side_length)
 
-print(f"Height: {height}")
-print(f"Base, first value: {base1}")
-print(f"Base, second value: {base2}")
-print(f"Expected Output: {area}")
+print(f"Input number of sides: {n}")
+print(f"Input the length of a side: {side_length}")
+print(f"The area of the polygon is: {area:.2f}")
