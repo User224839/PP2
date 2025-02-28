e11d9feba107e69697f316690c23055784b695be
User224839 <serikbajn18@gmail.com>
2025-02-28 12:45:57 +0500

    5.1.2

--- lab4/5.py
+++ lab4/5.py
@@ -5,7 +5,8 @@ def read_file():
         return file.read()
 
 data = read_file()
-def match_a_followed_by_b():
-    return re.findall(r'a*b*', data)
 
-print(match_a_followed_by_b())
+def match_a_followed_by_2_to_3_b():
+    return re.findall(r'a{1}b{2,3}', data)
+
+print(match_a_followed_by_2_to_3_b())
