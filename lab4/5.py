42f63a904bd5356aafbb342765e384b4d3d9bf9f
User224839 <serikbajn18@gmail.com>
2025-02-28 12:36:17 +0500

    5.1.10

--- lab4/5.py
+++ lab4/5.py
@@ -1,7 +1,7 @@
 import re
 
-def split_at_uppercase(string):
-    return re.split(r'(?=[A-Z])', string)
+def camel_to_snake(string):
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', string).lower()
 
 # Example usage:
-print(split_at_uppercase("HelloWorldTest"))  # ['Hello', 'World', 'Test']
+print(camel_to_snake("HelloWorldTest"))  # "hello_world_test"
