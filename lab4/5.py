9fc60bf8d2414b8786ff29346dfc79f02cd8173e
User224839 <serikbajn18@gmail.com>
2025-02-28 12:31:27 +0500

    5.1.2

--- lab4/5.py
+++ lab4/5.py
@@ -1,7 +1,7 @@
 import re
 
-def match_a_followed_by_b(string):
-    return re.fullmatch(r'a*b*', string) is not None
+def match_a_followed_by_2_to_3_b(string):
+    return re.fullmatch(r'a{1}b{2,3}', string) is not None
 
 # Example usage:
-print(match_a_followed_by_b("abbb"))  # True
+print(match_a_followed_by_2_to_3_b("abb"))  # True
