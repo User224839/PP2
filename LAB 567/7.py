2fdb9c12dc67726f637879096ed7ae33f4d82a9b
User224839 <serikbajn18@gmail.com>
2025-04-03 15:57:48 +0500

    7.1

--- LAB 567/7.py
+++ LAB 567/7.py
@@ -2,20 +2,15 @@ import pygame
 import time
 import math
 
-# Инициализация Pygame
 pygame.init()
 
-# Опции экрана
 WIDTH, HEIGHT = 400, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mickey Clock")
-
-# Загрузка изображения Микки
 mickey = pygame.image.load("C:\\Users\\Nurdaulet\\Desktop\\PP2\\LAB 567\\mickey.jpeg")
 mickey = pygame.transform.scale(mickey, (WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-# Функция для рисования стрелок
 def draw_hand(surface, angle, length, color, width=5):
     angle_rad = math.radians(angle)
     x = WIDTH // 2 + length * math.cos(angle_rad)
@@ -26,19 +21,15 @@ running = True
 while running:
     screen.fill((255, 255, 255))
     screen.blit(mickey, (0, 0))
-
-    # Получаем текущее время
     current_time = time.localtime()
     minutes = current_time.tm_min
     seconds = current_time.tm_sec
 
-    # Вычисляем углы поворота
     min_angle = 360 - (minutes % 60) * 6
     sec_angle = 360 - (seconds % 60) * 6
 
-    # Рисуем стрелки
-    draw_hand(screen, min_angle, 80, (0, 0, 255), 6)  # Минутная стрелка (правая рука)
-    draw_hand(screen, sec_angle, 100, (255, 0, 0), 4)  # Секундная стрелка (левая рука)
+    draw_hand(screen, min_angle, 80, (0, 0, 255), 6)  
+    draw_hand(screen, sec_angle, 100, (255, 0, 0), 4)  
 
     pygame.display.flip()
     clock.tick(30)
