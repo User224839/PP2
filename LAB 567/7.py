17af480d1c997cff59da3fced77175288b9ee033
User224839 <serikbajn18@gmail.com>
2025-04-03 16:11:41 +0500

    7.1

--- LAB 567/7.py
+++ LAB 567/7.py
@@ -1,12 +1,14 @@
 import pygame
 import time
 import math
+import os
 
 pygame.init()
 
 WIDTH, HEIGHT = 400, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mickey Clock")
+
 mickey = pygame.image.load("C:\\Users\\Nurdaulet\\Desktop\\PP2\\LAB 567\\mickey.jpeg")
 mickey = pygame.transform.scale(mickey, (WIDTH, HEIGHT))
 clock = pygame.time.Clock()
@@ -21,6 +23,7 @@ running = True
 while running:
     screen.fill((255, 255, 255))
     screen.blit(mickey, (0, 0))
+
     current_time = time.localtime()
     minutes = current_time.tm_min
     seconds = current_time.tm_sec
@@ -29,7 +32,7 @@ while running:
     sec_angle = 360 - (seconds % 60) * 6
 
     draw_hand(screen, min_angle, 80, (0, 0, 255), 6)  
-    draw_hand(screen, sec_angle, 100, (255, 0, 0), 4)  
+    draw_hand(screen, sec_angle, 100, (255, 0, 0), 4) 
 
     pygame.display.flip()
     clock.tick(30)
